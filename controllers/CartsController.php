db812e3bb357db5b50e2e79239eb2a064797ec7c
NikitaHodor <nikitahodor13@gmail.com>
2020-08-09 21:04:47 +0300

    иконки

--- controllers/CartsController.php
+++ controllers/CartsController.php
@@ -26,7 +26,7 @@
 				$cosmeticModel = new Cosmetic();
 				$cosmeticList = $cosmeticModel->getCosmeticListForOrder($cosmeticIdList);
 			}
-			echo $_COOKIE['user_id'];//отладка
+//			echo $_COOKIE['user_id'];//отладка
 			
 			include_once('./views/carts/index.php');
 		}
--- views/templates/header.php
+++ views/templates/header.php
@@ -4,15 +4,16 @@
 <head>
     <title> <?= $title; ?></title>
     <link rel="stylesheet" href="<?= LIBS ?>bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= CSS ?>main.css">
 </head>
 
 <body>
-		<nav class="navbar top navbar-expand-sm navbar-dark bg-dark">
+    <nav class="navbar top navbar-expand-sm navbar-dark bg-dark">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="navbar-header <?= ($title == 'Enso Cosmetics') ? 'active' : '' ?>">
                 <a class="navbar-brand" href="<?= SITE_ROOT . 'cosmetics/list' ?>">Enso Cosmetics</a>
             </div>
-		    <ul class="navbar-nav mr-auto">
+            <ul class="navbar-nav mr-auto">
                 <li class="nav-item <?= ($title == 'Каталог') ? 'active' : '' ?>">
                     <a class="nav-link" href="<?= SITE_ROOT . 'categories/list' ?>">Каталог</a>
                 </li>
@@ -26,19 +27,21 @@
                     <a class="nav-link" href="<?= SITE_ROOT . '' ?>">FAQs</a>
                 </li>
                 <li class="nav-item <?= ($title == 'Корзина') ? 'active' : '' ?>">
-                    <a class="nav-link" href="<?= SITE_ROOT . 'cart' ?>">Корзина</a>
+                    <a class="nav-link" href="<?= SITE_ROOT . 'cart' ?>"><i class="fa fa-shopping-cart"></i></a>
                 </li>
-		      <? if (User::checkIfUserAuthorized()) : ?>
-		      	<li class="nav-item">
-		        	<a class="nav-link" href="<?= SITE_ROOT . 'logout' ?>">Выход</a>
-		      	</li>
-		      <? else : ?>
-			      <li class="nav-item <?= ($title == 'Регистрация') ? 'active' : '' ?>">
-			        <a class="nav-link" href="<?= SITE_ROOT . 'register' ?>">Регистрация</a>
-			      </li>
-			      <li class="nav-item <?= ($title == 'Авторизация') ? 'active' : '' ?>">
-			        <a class="nav-link" href="<?= SITE_ROOT . 'auth' ?>">Авторизация</a>
-			      </li>
-			  <? endif; ?>
-		  </div>
-		</nav>
+                <? if (User::checkIfUserAuthorized()) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= SITE_ROOT . 'logout' ?>">Выход</a>
+                </li>
+                <? else : ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i></a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item <?= ($title == 'Регистрация') ? 'active' : '' ?>" href="<?= SITE_ROOT . 'register' ?>">Регистрация</a>
+                        <a class="dropdown-item <?= ($title == 'Авторизация') ? 'active' : '' ?>" href="<?= SITE_ROOT . 'auth' ?>">Авторизация</a>
+                    </div>
+                </li>
+
+                <? endif; ?>
+        </div>
+    </nav>
