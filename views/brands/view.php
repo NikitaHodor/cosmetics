227b316c52c3dbcd9eb19aca6bdcf40e8040ca74
NikitaHodor <nikitahodor13@gmail.com>
2020-09-03 14:51:48 +0300

    03.09

--- views/brands/view.php
+++ views/brands/view.php
@@ -7,9 +7,11 @@
     <div class="card-body">
         <h5 class="card-title"><?= $cosmetic['cosmetic_name']; ?></h5>
         <p class="card-text"><?= $cosmetic['cosmetic_description']; ?></p>
+	    <? if (User::checkIfUserAuthorized()) : ?>
         <div class="row">
             <a class="btn btn-primary" onclick="addToCart(<?= $cosmetic['cosmetic_id']; ?>)">Добавить в корзину </a>
         </div>
+        <? endif; ?>
         <? if (User::checkIfAdminAuthorized()) : ?>
         <a href="<?= SITE_ROOT . 'cosmetics/edit/' . $cosmetic['cosmetic_id']; ?>" class="btn btn-primary">Редактировать</a>
         <a class="btn btn-danger" onclick="deleteCosmetic(<?= $cosmetic['cosmetic_id']; ?>, '<?= SITE_ROOT; ?>')">Удалить</a>
