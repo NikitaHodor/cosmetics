79eda993a89e118cf6beca0f5b0a6b06642b7a30
NikitaHodor <nikitahodor13@gmail.com>
2020-09-21 10:24:28 +0300

    view

--- views/categories/view.php
+++ views/categories/view.php
@@ -1,25 +1,22 @@
 <? include_once('./views/templates/header.php'); ?>
 
-
-<? foreach ($categoryCosm as $cosmetic): ?>
-<div class="card" style="width: 18rem;">
-    <img src="<?= $cosmetic['image_url']; ?>" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title"><?= $cosmetic['cosmetic_name']; ?></h5>
-        <p class="card-text"><?= $cosmetic['cosmetic_description']; ?></p>
-	    <? if (User::checkIfUserAuthorized()) : ?>
-        <div class="row">
-            <a class="btn btn-secondary" onclick="addToCart(<?= $cosmetic['cosmetic_id']; ?>)">Добавить в корзину </a>
-        </div>
-        <? endif; ?>
-<!--
-        <? if (User::checkIfAdminAuthorized()) : ?>
-        <a href="<?= SITE_ROOT . 'cosmetics/edit/' . $cosmetic['cosmetic_id']; ?>" class="btn btn-primary">Редактировать</a>
-        <a class="btn btn-danger" onclick="deleteCosmetic(<?= $cosmetic['cosmetic_id']; ?>, '<?= SITE_ROOT; ?>')">Удалить</a>
-        <? endif; ?>
--->
-    </div>
-</div>
+<div class="container">
+   <div class="row">
+       <? foreach ($categoryCosm as $cosmetic): ?>
+<div class="card main_card">
+                    <img src="<?= $cosmetic['image_url'] ?>" class="card-img-top main_card_img" alt="card-img">
+                    <div class="card-body">
+                        <span class="card-text">
+                            <a href="<?= SITE_ROOT . 'cosmetics/view/' . $cosmetic['cosmetic_id'] ?>">
+                                <?= $cosmetic['cosmetic_name']; ?>
+                            </a>
+                        </span>
+                    </div>
+                </div>
 <? endforeach; ?>
+   </div>
+
+</div>
+
 
 <? include_once('./views/templates/footer.php'); ?>
--- views/cosmetics/view.php
+++ views/cosmetics/view.php
@@ -1,23 +1,22 @@
 <? include_once('./views/templates/header.php'); ?>
 
-
-	<div class="card" style="width: 18rem;">
-  <img src=" <?= $cosmetic['image_url']; ?>" class="card-img-top" alt="...">
-	  <div class="card-body">
-	    <h5 class="card-title"><?= $cosmetic['cosmetic_name']; ?></h5>
+<div class="card mb-3">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="<?= $cosmetic['image_url']; ?>" class="card-img" alt="card-img">
+    </div>
+    <div class="col-md-8 my-auto">
+      <div class="card-body">
+        <h5 class="card-title"><?= $cosmetic['cosmetic_name']; ?></h5>
 	    <p class="card-text"><?= $cosmetic['cosmetic_description']; ?></p>
 	    <? if (User::checkIfUserAuthorized()) : ?>
 	    <div class="row"> 
 	    	<a class="btn btn-secondary" onclick="addToCart(<?= $cosmetic['cosmetic_id']; ?>)">Добавить в корзину </a>
 	    </div>
 	    <? endif; ?>
-<!--
-	    <? if (User::checkIfAdminAuthorized()) : ?>
-		    <a href="<?= SITE_ROOT . 'cosmetics/edit/' . $cosmetic['cosmetic_id']; ?>" class="btn btn-primary">Редактировать</a>
-		    <a class="btn btn-danger" onclick="deleteCosmetic(<?= $cosmetic['cosmetic_id']; ?>, '<?= SITE_ROOT; ?>')">Удалить</a>
-		<? endif; ?>
--->
-	  </div>
-	</div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <? include_once('./views/templates/footer.php'); ?>
